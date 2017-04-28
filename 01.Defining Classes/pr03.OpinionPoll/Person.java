81bf21838984eb0828737f702d646a42d8ce1931
MBenina <magdalina.benina@gmail.com>
2017-04-28 15:18:15 +0300

    Update Person.java

--- 01.Defining Classes/pr03.OpinionPoll/Person.java
+++ 01.Defining Classes/pr03.OpinionPoll/Person.java
@@ -1,43 +1,50 @@
-package pr03.OpinionPoll;
+package asdasd;
 
-public class Person 
+
+public class Person
 {
-	
-	private String name;
-	private int age;
-	
-	public Person(String name, int age) 
-	{
-		this.name = name;
-		this.age = age;
-	}
-	
-	public String getName()
-	{
-		return this.name;
-	}
-	
-	public void setName(String name)
-	{
-		this.name = name;
-	}
-	
-	public int getAge()
-	{
-		return this.age;
-	}
-	
-	public void setAge(int age)
-	{
-		this.age = age;
-	}
-	
-	@Override
-    public String toString()
-    {
-        StringBuilder sb = new StringBuilder();
-        sb.append(this.getName() + " " + this.getAge());
+        private String name;
+        private int age;
 
-        return sb.toString();
-    }
+        public Person(String name, int age)
+        {
+            setName(name);
+            setAge(age);
+        }
+
+        public String getName()
+        {
+            return this.name;
+        }
+
+        public void setName(String name)
+        {
+            if (!name.isEmpty() && name.length() > 1) {
+                this.name = name;
+            } else {
+                throw new IllegalArgumentException("Name should not be empty and must be atleast 2 symbols long.");
+            }
+        }
+
+        public int getAge()
+        {
+            return this.age;
+        }
+
+        public void setAge(int age)
+        {
+            if (age < 0 || age > 110) {
+                throw new IllegalArgumentException("Age should be between 0 and 110.");
+            }
+            this.age = age;
+        }
+
+        @Override
+        public String toString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.append(this.getName() + " " + this.getAge());
+
+            return sb.toString();
+        }
 }
