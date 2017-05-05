d51a29488fe1c857f21018aac90a669102ddee30
MBenina <magdalina.benina@gmail.com>
2017-05-05 16:26:13 +0300

    Update Truck.java

--- 04.Polymorphism/Vehicles/Truck.java
+++ 04.Polymorphism/Vehicles/Truck.java
@@ -3,17 +3,32 @@ package pr1.Vehicles;
 public class Truck extends Vehicle
 {
 
-	protected Truck(String vehicleName, Double fuelQuantity, Double litersPerKm) 
+	protected Truck(Double fuelQuantity, Double fuelConsumptionPerKm)
 	{
-		super(vehicleName, fuelQuantity, litersPerKm);
+		super(fuelQuantity, fuelConsumptionPerKm);
 	}
 
 	@Override
-	public void calFuelConsumption() {
+	public void driveDistance(Double distance) 
+	{
 		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void refuel(Double fuelQuantity) 
+	{
+		Double fuelConsumption = super.getFuelConsumptionPerKm();
+		double fuelToTravellDistance = distance * fuelConsumption;
 		
+        if (fuelToTravellDistance > fuelQuantity)
+        {
+            throw new InvalidOperationException("Truck needs refueling");
+        }
+        fuelQuantity -= fuelToTravellDistance;
+
 	}
 
 
-	
+
 }
